ea32f36a226d6ed067cc4248c49d208bdd5e1357
FreshMen126 <creepergame126@gmail.com>
2021-03-26 17:42:32 +0300

    Создание основных .h файлов
    Создал main.h, в который потом будут скидывать другие .h файлы. Также константная библиотека для констант, которые будут использоваться для удобства

---
+++ src/constant.h
@@ -0,0 +1,4 @@
+#define byte uint8_t    //байт
+#define LED_PIN 13      //светодиод на 13 линии
+#define second 1000     //секунда
+#define minute 60000    //минута
---
+++ src/main.h
@@ -0,0 +1,3 @@
+#include <Arduino.h>
+#include "Roomba.h"
+#include "constant.h"
