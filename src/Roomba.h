3abec0d930b2b7ee5f4b6a5db54c473dfe641783
FreshMen126 <creepergame126@gmail.com>
2021-04-15 15:52:15 +0300

    Музыка и исправления
    Добавлена последняя мелодия и оптимизирована команда Stop (теперь она может стопать движение, проигрывать музыку и стопать саму румбу) и добавлены комментарии
    
    Началась работа с сенсорами

--- src/Roomba.h
+++ src/Roomba.h
@@ -13,6 +13,7 @@ void Roomba_Stop();      //выключает румбу
 void Roomba_Set_LED(bool debrisLED, bool spotLED, bool dockLED, bool checkLED, byte color, byte intensity);
 sbyte2 Roomba_Compare(sbyte2 compare, sbyte2 min, sbyte2 max);  //команда ограничения между max и min
 void Roomba_Go(sbyte2 velocity, sbyte2 radius); //Команда езды румбы со скоростью и радиусом 
+void Roomba_GoDirect(sbyte2 right, sbyte2 left); //управление движением колес вперед и назад
 void Roomba_Play_Song(byte song);    //команда запуска музыки от 0 до 4
 void Roomba_Init_Song();         //команда сохраняющая в памяти музыку (инициализация)
 /*
