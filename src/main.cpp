fec979f3df79c278623cf2875eb042d606cdb439
FreshMen126 <creepergame126@gmail.com>
2021-06-05 16:12:40 +0300

    Реализация таймера
    добавлена библиотека, которая упрощает реализацию таймера (avr)

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,32 @@
 #include "main.h"
 #include "stdio.h"
+#include "deps/GyverTimers.h"
 
 void setup()
 {
   pinMode(RX_PIN, OUTPUT);
   pinMode(TX_PIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
-  //Serial.begin(9600);
+  Serial.begin(9600);
+  Timer1.setPeriod(5000);
+  Timer1.enableISR();
+  /*
+  cli();                 // отключить глобальные прерывания
+  TCCR0A = 0;            // установить TCCR1A регистр в 0
+  TCCR0B = 0;            // установить TCCR1B регистр в 0
+  TIMSK0 = (1 << TOIE0); // включить прерывание Timer1 overflow
+  TCCR0B |= (1 << CS02) | (1 << CS01) | (1 << CS00); // Установить  биты ()
+  sei();                   // включить глобальные прерывания
+  */
   roomba_Init();
 }
-/*
-unsigned long long a = 0;
-int timing1 = 0;
-typedef enum
-{
-  BackDrive1,
-  ForwardDrive1
-} StatusDrive1; 
-StatusDrive1 statusDrive1 = ForwardDrive1;
-*/
+
 void loop()
 {
-  /*
-  if (statusDrive1 == ForwardDrive1)
-  {
-    Roomba_GoDirect(50, 50);
-    if (millis() >= a)
-    {
-      a = millis() + 3000;
-      statusDrive1 = BackDrive1;
-    }
-  }
-  else if (statusDrive1 == BackDrive1)
-  {
-    Roomba_GoDirect(-50, -50);
-    if (millis() >= a)
-    {
-      a = millis() + 3000;
-      statusDrive1 = ForwardDrive1;
-    }
-  }
-  if (millis() - timing1 > 5)
-  {
-    timing1 = millis();
-    if (Roomba_Sensors_Pack_Check())
-    {
-    }
-  }
-  */
   roomba_Loop();
+}
+
+ISR(TIMER1_A) 
+{
+  roomba_SensorsLoop();
 }
